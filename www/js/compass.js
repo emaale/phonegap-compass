d2b1d859fe2564c09985b57bbfc7090db9f1c0f8
Manne <manne@Mannes-MacBook-Pro.local>
2015-11-30 23:10:05 -0600

    Modified compass.js

--- www/js/compass.js
+++ www/js/compass.js
@@ -6,7 +6,7 @@ function onDeviceReady() {
 
     // Options for the navigator
     var options = {
-        frequency: 3000
+        frequency: 50
     }; // Update every 3 seconds
 
     var watchID = navigator.compass.watchHeading(onSuccess, onError, options);
@@ -19,3 +19,20 @@ function onSuccess(heading) {
 function onError(compassError) {
     alert('Compass error: ' + compassError.code);
 };
+
+function drawCompass(angle) {
+	// Properties for the compass
+	var compass = {
+		background: "#2ecc71",
+		width: 40,
+		color: {
+			north: "#e74c3c",
+			south: "#ecf0f1"
+		}
+	};
+
+	// Display degrees
+	document.querySelector(".example").innerHTML = "&deg;" + angle;
+
+	// Draw the compass with the provided angle
+}
