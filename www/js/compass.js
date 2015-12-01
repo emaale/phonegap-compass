f1e1f78eb7213230e9ff642ecc0cf8a0aa9e470e
Manne <manne@Mannes-MacBook-Pro.local>
2015-11-30 22:55:32 -0600

    Modified compass.js

--- www/js/compass.js
+++ www/js/compass.js
@@ -1,1 +1,21 @@
-alert("Testing JS file");
+// Add deviceready event
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Function that runs when device is ready
+function onDeviceReady() {
+
+    // Options for the navigator
+    var options = {
+        frequency: 3000
+    }; // Update every 3 seconds
+
+    var watchID = navigator.compass.watchHeading(onSuccess, onError, options);
+}
+
+function onSuccess(heading) {
+    alert(heading.magneticHeading);
+};
+
+function onError(compassError) {
+    alert('Compass error: ' + compassError.code);
+};
